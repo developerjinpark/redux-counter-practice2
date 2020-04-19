2c8126a303e073d24c29460fb1ccfcb6ddcdea50
Jin <jinpark1504@gmail.com>
2020-04-18 21:57:13 -0400

    comment out code of CounterContainer

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,25 +1,25 @@
-import Counter from 'components/Counter';
-import * as actions from 'actions';
-import { connect } from 'react-redux';
-import getRandomColor from 'lib/getRandomColor';
+// import Counter from 'components/Counter';
+// import * as actions from 'actions';
+// import { connect } from 'react-redux';
+// import getRandomColor from 'lib/getRandomColor';
 
-const mapStateToProps = (state) => ({
-    color: state.colorData.color,
-    number: state.numData.number
-});
+// const mapStateToProps = (state) => ({
+//     color: state.color,
+//     number: state.number
+// });
 
-const mapDispatchToProps  = (dispatch) => ({
-    onIncrement: () => dispatch(actions.increment()),
-    onDecrement: () => dispatch(actions.decrement()),
-    onSetColor: () => {
-        const color = getRandomColor();
-        dispatch(actions.setColor(color));
-    }
-});
+// const mapDispatchToProps  = (dispatch) => ({
+//     onIncrement: () => dispatch(actions.increment()),
+//     onDecrement: () => dispatch(actions.decrement()),
+//     onSetColor: () => {
+//         const color = getRandomColor();
+//         dispatch(actions.setColor(color));
+//     }
+// });
 
-const CounterContainer = connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(Counter);
+// const CounterContainer = connect(
+//     mapStateToProps,
+//     mapDispatchToProps
+// )(Counter);
 
-export default CounterContainer;
+// export default CounterContainer;
